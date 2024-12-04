28b63716b29dc6c1f0e8dec8d166ced3078cbd08
Arthur Felipe <92448606+arthurfaraujo@users.noreply.github.com>
2024-12-04 16:13:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const countries = await fetch('https://raw.githubusercontent.com/hampusborgos/country-flags/refs/heads/main/countries.json').then(res => res.json())
+
+const body = document.querySelector('.flags')
+
+function genFlagCard(country) {
+    const div = document.createElement('div')
+    div.className = "flag"
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+
+    img.src = `https://raw.githubusercontent.com/hampusborgos/country-flags/refs/heads/main/png100px/${country.toLowerCase()}.png`
+    span.innerHTML = countries[country]
+    div.appendChild(img)
+    div.appendChild(span)
+
+    body.appendChild(div)
+}
+
+
+for (const country in countries) {
+    genFlagCard(country)
+}
